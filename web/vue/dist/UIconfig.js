6721336ca731e39433435b715f6cf7059d77c2b2
jgdesert <jg.desert@free.fr>
2020-02-18 12:43:01 +0000

    developpement docker config

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -3,18 +3,17 @@
 
 // This config is used by both the frontend as well as the web server.
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
-
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
-    port: 3000,
+    host: '188.166.35.182',
+    port: 5000,
     path: '/'
   },
   adapter: 'sqlite'
