807367c6bfb29720ae203ff1415075a52bf58cc7
jgdesert <jg.desert@dfree.fr>
2020-03-02 13:20:05 +0100

    myStrat-5 update

--- strategies/myStrat-5-USD-BTC-1.js
+++ strategies/myStrat-5-USD-BTC-1.js
@@ -25,6 +25,18 @@ strat.init = function(candle) {
      this.period = this.settings.period;
      this.URI = this.settings.uri;
      //
+     //
+     this.price = 0;
+     this.unix = 0;
+     this.paperFormat = '';
+     this.formatDate = '';
+     //
+     this.sellat = 0;
+     this.stop_loss_pct = 0;
+     //
+     this.threshold = "";
+     this.stop = "";
+     this.stop_up = "";
 
      //papers
      this.dateLong = '';
@@ -37,15 +49,14 @@ strat.init = function(candle) {
      //Global Trigger
      this.triggerIndex = 0;
      //sell
-     this.pallier = 0
+     this.counterSell = 0
      //buy
-     this.compteur = 0;
+     this.counterBuy = 0;
 
-     //
+     //Indicator
      this.percentBetweenLastCandle = 0;
      this.percentBetweenPriceAndMa = 0;
      //
-     this.triggerSell = 0;
      this.lastPrice = 0;
      //
      this.memory1 = 0
@@ -65,10 +76,23 @@ strat.log = function() {
 // update or not.
 strat.check = function(candle) {
 
+  // START STRAGEGIE
+  this.triggerIndex++;
+  console.log(this.triggerIndex);
+
+  //Util Var
+  var date = new Date(candle.start._d);
+  this.price = candle.close;
+  this.unix = moment(candle.start._d).unix();
+  this.paperFormat = moment(date).format('YYYY-MM-DD HH:mm')
+  this.formatDate = moment(date).format();
+
 /****************************************************************************************************************************/
 /****************************************************************************************************************************/
 //Import RequestApp + Indicator/*********************************************************************************************/
+  //
   var requestApp = myLibrary.requestFunctionApp(this.URI);
+  var requestOrder = myLibrary.requestOrder(this);
   //
   var myIndicatorOSI = myLibrary.addOverlapStudiesIndicator(this.talibIndicators);
   var myIndicatorM = myLibrary.addMomentumIndicator(this.talibIndicators);
@@ -77,102 +101,39 @@ strat.check = function(candle) {
   var myIndicatorPT = myLibrary.addPriceTransformIndicator(this.talibIndicators);
   var myIndicatorC = myLibrary.addCycleIndicator(this.talibIndicators);
   var myFrontAppIndicator = myLibrary.addFrontAppIndicator(this.talibIndicators);
-  console.log(myFrontAppIndicator);
 
-/****************************************************************************************************************************/
-  function sellAction(self){
-    self.threshold = "";
-    self.stop = "";
-    self.stop_up = ""
-
-
-   self.advice('short');
-   self.pallier = 0
-   self.currentTrend = 'short';
-   //log SELL
-   log.debug('                                                                                                     SELL');
-   //Log Trades App
-   /*********************************************************************************************************************/
-   log.debug('makeHttpRequestTrade short');
-   if(self.mode === 'backtest'){requestApp.backtest.trade("TRADE",self.backtestId,self.triggerIndex,formatDate,price,'short')}
-   if(self.mode === 'live'){requestApp.live.trade("LIVETRADE",self.backtestId,self.triggerIndex,unix.toString(),price,'short')}
-   /****************************************************************************************************************/
-   //PAPER TRADER
-   self.currentPrixVente = candle.close;
-   self.dateShort = paperFormat;
-   log.debug('makeHttpRequestPaper paper');
-   if(self.mode === 'backtest'){
-     requestApp.backtest.paper("PAPER",self.backtestId,self.triggerFirstTrade,self.dateLong,self.dateShort,self.currentPrixAchat,self.currentPrixVente);
-   };
-   if(self.mode === 'live'){
-     requestApp.live.paper("LIVEPAPER",self.backtestId,self.triggerFirstTrade,self.dateLong,self.dateShort,self.currentPrixAchat,self.currentPrixVente);
-     //alert + record Db Mongo
-     requestApp.signal.alert("LIVESIGNAL",self.backtestId,self.backtestId,self.triggerIndex,self.strat,self.exchange,self.currency,self.asset,self.period,unix.toString(),price,'short');
-     //alert AllSignals
-     requestApp.signal.alert("LIVESIGNAL","AllSignals",self.backtestId,self.triggerIndex,self.strat,self.exchange,self.currency,self.asset,self.period,unix.toString(),price,'short');
-  };
-   /******************************************************************************************************************/
-   log.debug('                                                                                                     SELL');
-}
-  function buyAction(self){
-//
-self.currentPrixAchat = candle.close;
-self.triggerFirstTrade++;
-self.dateLong = paperFormat;
-//New Objectif
-self.threshold = self.currentPrixAchat * sellat;
-self.stop = self.currentPrixAchat * stop_loss_pct;
-self.stop_up = self.threshold;
-
-
-/******************************************************************************************************************************************************/
-self.advice('long');
-self.compteur = 0
-self.currentTrend = 'long';
-//log BUY
-log.debug('                                                                                                      BUY');
-//Log Trades App
-log.debug('makeHttpRequestTrade long');
-if(self.mode === 'backtest'){requestApp.backtest.trade("TRADE",self.backtestId,self.triggerIndex,formatDate,price,'long')}
-if(self.mode === 'live'){
-  requestApp.live.trade("LIVETRADE",self.backtestId,self.triggerIndex,unix.toString(),price,'long');
-  //alert + record Db Mongo
-  requestApp.signal.alert("LIVESIGNAL",self.backtestId,self.backtestId,self.triggerIndex,self.strat,self.exchange,self.currency,self.asset,self.period,unix.toString(),price,'long');
-  //alert AllSignals
-  requestApp.signal.alert("LIVESIGNAL","AllSignals",self.backtestId,self.triggerIndex,self.strat,self.exchange,self.currency,self.asset,self.period,unix.toString(),price,'long');
-
-}
-log.debug('                                                                                                      BUY');
-}
 /****************************************************************************************************************************/
 // Variables + UI App /******************************************************************************************************/
 /****************************************************************************************************************************/
-    function test4(x){
-  if (x % 4 == 0)
-  {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-  //Util Var
-  var date = new Date(candle.start._d);
-  var unix = moment(candle.start._d).unix();
-  var formatDate = moment(date).format();
-  var paperFormat = moment(date).format('YYYY-MM-DD HH:mm')
-  var price = candle.close;
-
+  //console.log(this.unix*1000, parseInt(this.to));
   // export Request Function
   var percent = ((parseInt(new Date(date).getTime()) - parseInt(this.from) ) * 100) / ( parseInt(this.to) - parseInt(this.from) )
   if(this.mode === 'backtest'){
+    //
+    function test50(x){
+          if (x % 50 == 0)
+          {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+    if(this.period === '5' && test50(this.triggerIndex)){
       requestApp.backtest.percent("CPU",this.backtestId,percent)
+    } else {
+      requestApp.backtest.percent("CPU",this.backtestId,percent)
+    }
+    //
+    var calculSeconde = 60 * parseInt(this.period) * 1000
+    if(this.unix*1000 === parseInt(this.from) ){requestApp.backtest.trade("TRADE",this.backtestId,this.triggerIndex,this.formatDate,this.price,'start')}
+    if(this.unix*1000 === parseInt(this.to)-calculSeconde ){requestApp.backtest.trade("TRADE",this.backtestId,this.triggerIndex,this.formatDate,this.price,'end')}
+
     //console.log(parseFloat(percent.toFixed()));
   }
   if(this.mode === 'live'){
       var obj = {
-       "date" : unix.toString(),
+       "date" : this.unix.toString(),
        "price" : candle.close,
        "volume" : candle.volume,
        "trades" : candle.trades,
@@ -193,108 +154,77 @@ log.debug('                                                                     
      requestApp.live.data("LIVEDATA",this.backtestId,obj)
   }
 
-
-/****************************************************************************************************************************/
-/****************************************************************************************************************************/
-/****************************************************************************************************************************/
 /****************************************************************************************************************************/
 /****************************************************************************************************************************/
 /****************************************************************************************************************************/
 // Edit your Variables Strategies! /*****************************************************************************************/
 /****************************************************************************************************************************/
 
-    //threshold
+
     // calculate the minimum price in order to sell
-    const sellat = 1.01;
-    //short
-    const stop_loss_pct = 0.90;
+      //threshold
+      this.sellat = 1.01;
+      //stop_loss
+      //short
+      this.stop_loss_pct = 0.90;
+
+/****************************************************************************************************************************/
+/****************************************************************************************************************************/
 
 
-  /****************************************************************************************************************************/
-  /****************************************************************************************************************************/
-  /****************************************************************************************************************************/
-  // START STRAGEGIE
-    this.triggerIndex++;
-    console.log(this.triggerIndex);
 
     //
     function getPercentageChange(oldNumber, newNumber){
       var decreaseValue = oldNumber - newNumber;
       return (decreaseValue / oldNumber) * 100;
     }
-    this.percentBetweenLastCandle = getPercentageChange(this.lastPrice,price)
-    this.percentBetweenPriceAndMa = getPercentageChange(myFrontAppIndicator.SMA20,price)
+    this.percentBetweenLastCandle = getPercentageChange(this.lastPrice,this.price)
+    this.percentBetweenPriceAndMa = getPercentageChange(myFrontAppIndicator.SMA20,this.price)
     //console.log('percentBetweenLastCandle :',this.percentBetweenLastCandle);
-    //console.log('percentBetweenPriceAndMa :',this.percentBetweenPriceAndMa);
+    console.log('percentBetweenPriceAndMa :',this.percentBetweenPriceAndMa);
 
     //cupute last 3 candles
     this.memory3 = this.percentBetweenPriceAndMa
     var totalLastMemory = this.memory1 + this.memory2 + this.memory3
     //console.log(totalLastMemory);
 
-  /****************************************************************************************************************************/
-  /****************************************************************************************************************************/
-  /****************************************************************************************************************************/
-  /****************************************************************************************************************************/
+/****************************************************************************************************************************/
+/****************************************************************************************************************************/
+/****************************************************************************************************************************/
+/****************************************************************************************************************************/
 
-        /// Setting conditions
-        if (this.currentTrend === 'long' ) {
-            if(this.pallier === 0){
-              this.triggerSell = 0
-            }
-            // Start
-            this.pallier++;
+          /// Setting conditions
+            if (this.currentTrend === 'long' ) {
 
-            //Signal Pallier
+                //reset
+                if(this.counterSell === 0){}
 
-
-            if (price > this.threshold){
-                this.triggerSell = 1;
-
-                this.stop_up = price * sellat;
-                this.stop =  this.threshold * stop_loss_pct
-                this.threshold = this.stop_up;
-                //Log Trades App
-                log.debug('makeHttpRequestTrade pallier');
-                if(this.mode === 'backtest'){requestApp.backtest.trade("TRADE",this.backtestId,this.triggerIndex,formatDate,price,'pallier')}
-                if(this.mode === 'live'){requestApp.live.trade("LIVETRADE",this.backtestId,this.triggerIndex,unix.toString(),price,'pallier')}
-
-                /*
-                if( totalLastMemory < -40){
-                  sellAction(this);
+                //start
+                this.counterSell++;
+                console.log(this.counterSell);
+                /****************************************************************************************************************/
+                //condition
+                if(this.percentBetweenPriceAndMa > 2 ){
+                  //SHORT position !!!
+                  requestOrder.sell(this,requestApp);
                 }
-                */
-
 
             }
+            else if (this.currentTrend === 'short') {
 
-            /****************************************************************************************************************/
-            //Signal SHORT Position !!!
-            if( price >  myFrontAppIndicator.SMA10 && price >  myFrontAppIndicator.SMA20  && this.triggerSell === 0  ){
-              sellAction(this);
+                //reset
+                if(this.counterBuy === 0){}
+
+                //start
+                this.counterBuy++;
+                /***********************************************************/
+                if (myFrontAppIndicator.SMA10>myFrontAppIndicator.SMA20 && myFrontAppIndicator.SMA20> myFrontAppIndicator.SMA50 && myFrontAppIndicator.SMA50>myFrontAppIndicator.SMA100){
+                  //LONG position !!!
+                  requestOrder.buy(this,requestApp);
+                }
             }
 
-            this.triggerSell = 0
-        }
-        else if (this.currentTrend === 'short') {
-              if(this.compteur === 0){
-
-              }
-             //Start
-             this.compteur++;
-             /***********************************************************/
-             //Signal LONG Position !!!
-             // #1
-             if (this.lastPrice > price && this.compteur++ > 288 ){
-               //Here to LONG position !
-               buyAction(this);
-             }
-
-
-
-        }
-
-  this.lastPrice = price;
+  this.lastPrice = this.price;
   this.memory1 = this.memory2;
   this.memory2 = this.memory3;
   this.memory3 = 0;
